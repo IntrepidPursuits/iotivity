5bf67c9082869a39bfe42f1b3aacd1cc78098c4a
PhilIntrepid <pdc52@cornell.edu>
2015-06-04 14:39:33 -0400

    Initial main screen ui (WIP)

---
+++ android_studio/app/src/main/java/org/iotivity/ca/ThreadMainActivity.java
@@ -0,0 +1,18 @@
+package org.iotivity.ca;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.threadgroup.iotivity.R;
+
+/**
+ * Created by philipcorriveau on 6/4/15.
+ */
+public class ThreadMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread_main);
+    }
+}
